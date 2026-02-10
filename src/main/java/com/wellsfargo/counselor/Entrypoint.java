07339ef82697961c15234a2170262c45dd37d2e1
gabrieleamodio <c.g.amodio@gmail.com>
2026-02-10 15:34:45 +0000

    Update Entrypoint.java

--- src/main/java/com/wellsfargo/counselor/Entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/Entrypoint.java
@@ -1,4 +1,4 @@
-package com.wellsfargo.counselor;
+package com.wellsfargo.counselor.entity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
